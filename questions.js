7461e73ef8402c4deec2030e9846d0e83fba01df
Bailey Garrett <baileyagarrett@gmail.com>
2019-10-30 16:59:54 -0500

    external sheets added, read me modified, added css sheet

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "HTML is what type of language?",
+    choices: ["Scripting Language", "Markup Language", "Programming Language", "Network Protocol"],
+    answer: "Markup Language"
+  }
+    {
+    title: "To reference a style sheet across multiple HTML pages, how would you define your CSS?",
+    choices: ["Inline Style", "Internal Style Sheet", "External Style Sheet", "CSS is meant for only one page"],
+    answer: "External Style Sheet"
+  },
+    {
+    title: "What does CSS stand for?",
+    choices: ["Computing Style Sheet", "Creative Style Sheet", "Cascading Style Sheet", "Creative Style System"],
+    answer: "Cascading Style Sheet"
+  },
+];
+
