75b1b7ef35c0c575b94af3c7a766d171ffd030bf
Bailey Garrett <baileyagarrett@gmail.com>
2019-10-31 16:12:06 -0500

    added content to landing page and relative css

--- assets/questions.js
+++ assets/questions.js
@@ -13,8 +13,8 @@ var questions = [
     title: "HTML is what type of language?",
     choices: ["Scripting Language", "Markup Language", "Programming Language", "Network Protocol"],
     answer: "Markup Language"
-  }
-    {
+  },
+  {
     title: "To reference a style sheet across multiple HTML pages, how would you define your CSS?",
     choices: ["Inline Style", "Internal Style Sheet", "External Style Sheet", "CSS is meant for only one page"],
     answer: "External Style Sheet"
---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function startQuiz() {
+  var content = document.getElementById("beginPage");
+  if (content.display === "none") {
+    content.display = "block";
+  } else {
+    content.display = "none";
+  }
+}
